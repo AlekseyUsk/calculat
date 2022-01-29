5ab115d2a5320a972c3450f939e170b6b624bfcf
Алексей <93115953+AlekseyUsk@users.noreply.github.com>
2022-01-29 19:27:20 +0700

    calculator_button_calculate

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button one = findViewById(R.id.one);
+        Button twoo = findViewById(R.id.twoo);
+        Button three = findViewById(R.id.three);
+        Button division = findViewById(R.id.division);  //деление
+        Button four = findViewById(R.id.four);
+        Button five = findViewById(R.id.five);
+        Button six = findViewById(R.id.six);
+        Button multiply = findViewById(R.id.multiply);  //умножение
+        Button seven = findViewById(R.id.seven);
+        Button eight = findViewById(R.id.eight);
+        Button nine = findViewById(R.id.nine);
+        Button sum = findViewById(R.id.sum);            //сложение
+        Button zero = findViewById(R.id.zero);
+        Button result = findViewById(R.id.result);
+        Button cha = findViewById(R.id.cha);            //дробные " , "
+        Button min = findViewById(R.id.min);            //вычитание
     }
 }
